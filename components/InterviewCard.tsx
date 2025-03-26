1f305c832a843fb2fb9a4359fb1e1e59230fc60d
SenArnab <senarnab414@gmail.com>
2025-03-26 23:01:01 +0530

    Implement InterviewCard

---
+++ components/InterviewCard.tsx
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs'
+
+const InterviewCard = ({ interviewId,
+                           userId,
+                           role,
+                           type,
+                           techstack,
+                           createdAt, } : InterviewCardProps) => {
+    const feedback = null as Feedback | null;
+    const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+    const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
+
+    return (
+        <div className="card-border w-[360px] max-sm:w-full min-h-96">InterviewCard</div>
+    )
+}
+export default InterviewCard
