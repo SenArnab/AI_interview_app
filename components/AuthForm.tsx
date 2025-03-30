248aa5a01167872637b5460fab548ecd285ea2cb
SenArnab <senarnab414@gmail.com>
2025-03-30 19:44:17 +0530

    Implement sign-in in AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,8 +12,9 @@ import Link from "next/link";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
-import {createUserWithEmailAndPassword} from "@firebase/auth";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "@firebase/auth";
 import {auth} from "@/firebase/client";
+import {signIn} from "@/lib/actions/auth.action";
 
 
 const authFormSchema = (type: FormType) => {
@@ -61,6 +62,20 @@ const AuthForm = ({ type } : {type: FormType}) => {
                 toast.success("Account created successfully. Please sign in.");
                 router.push("/sign-in");
             } else {
+                const { email, password } = values;
+
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                const idToken = await userCredential.user.getIdToken();
+
+                if (!idToken) {
+                    toast.error('SIgn in failed')
+                    return;
+                }
+
+                await signIn({
+                    email, idToken
+                })
+
                 toast.success("Sign in successful");
                 router.push("/");
             }
