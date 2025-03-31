bff1d142889aa6847a03dff3fcce3a44f38fcb28
SenArnab <senarnab414@gmail.com>
2025-03-31 21:33:42 +0530

    Implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,17 +5,15 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form} from "@/components/ui/form"
 import Image from "next/image";
 import Link from "next/link";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "@firebase/auth";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 import {auth} from "@/firebase/client";
-import {signIn} from "@/lib/actions/auth.action";
-
+import {signIn, signUp} from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -25,11 +23,10 @@ const authFormSchema = (type: FormType) => {
     })
 }
 
-const AuthForm = ({ type } : {type: FormType}) => {
+const AuthForm = ({ type }: { type: FormType }) => {
     const router = useRouter();
     const formSchema = authFormSchema(type);
 
-    // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -39,15 +36,14 @@ const AuthForm = ({ type } : {type: FormType}) => {
         },
     })
 
-    // 2. Define a submit handler.
     async function onSubmit(values: z.infer<typeof formSchema>) {
-        try{
-            if (type === "sign-up") {
+        try {
+            if(type === 'sign-up') {
                 const { name, email, password } = values;
 
                 const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
 
-                const result = await sigUp({
+                const result = await signUp({
                     uid: userCredentials.user.uid,
                     name: name!,
                     email,
@@ -56,19 +52,20 @@ const AuthForm = ({ type } : {type: FormType}) => {
 
                 if(!result?.success) {
                     toast.error(result?.message);
-                    return
+                    return;
                 }
 
-                toast.success("Account created successfully. Please sign in.");
-                router.push("/sign-in");
+                toast.success('Account created successfully. Please sign in.');
+                router.push('/sign-in')
             } else {
                 const { email, password } = values;
 
                 const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
                 const idToken = await userCredential.user.getIdToken();
 
-                if (!idToken) {
-                    toast.error('SIgn in failed')
+                if(!idToken) {
+                    toast.error('Sign in failed')
                     return;
                 }
 
@@ -76,21 +73,27 @@ const AuthForm = ({ type } : {type: FormType}) => {
                     email, idToken
                 })
 
-                toast.success("Sign in successful");
-                router.push("/");
+                toast.success('Sign in successfully.');
+                router.push('/')
             }
         } catch (error) {
             console.log(error);
-            toast.error(`There was an error: ${error}`);
+            toast.error(`There was an error: ${error}`)
         }
     }
 
     const isSignIn = type === 'sign-in';
+
     return (
         <div className="card-border lg:min-w-[566px]">
             <div className="flex flex-col gap-6 card py-14 px-10">
                 <div className="flex flex-row gap-2 justify-center">
-                    <Image src="/logo.svg" alt="Logo" height={32} width={38} />
+                    <Image
+                        src="/logo.svg"
+                        alt="logo"
+                        height={32}
+                        width={38}
+                    />
                     <h2 className="text-primary-100">PrepWise</h2>
                 </div>
 
@@ -103,14 +106,14 @@ const AuthForm = ({ type } : {type: FormType}) => {
                                 control={form.control}
                                 name="name"
                                 label="Name"
-                                placeholder="Enter your name"
+                                placeholder="Your Name"
                             />
                         )}
                         <FormField
                             control={form.control}
                             name="email"
                             label="Email"
-                            placeholder="Enter your email address"
+                            placeholder="Your email address"
                             type="email"
                         />
 
@@ -122,13 +125,14 @@ const AuthForm = ({ type } : {type: FormType}) => {
                             type="password"
                         />
 
-                        <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Create an account'}</Button>
+                        <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
                     </form>
                 </Form>
+
                 <p className="text-center">
-                    {isSignIn ? "No account yet?" : "Have an account already?"}
+                    {isSignIn ? 'No account yet?' : 'Have an account already?'}
                     <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
-                        {!isSignIn ? "Sign in" : "Sign up"}
+                        {!isSignIn ? "Sign in" : 'Sign up'}
                     </Link>
                 </p>
             </div>
